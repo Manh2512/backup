e5c2941cca46880576213c7750584a5642421cf0
Manh2512 <152011888+Manh2512@users.noreply.github.com>
2024-10-31 21:03:44 +0800

    Create ynet_with_gan-simplified.py

---
+++ first/ynet_with_gan-simplified.py
@@ -0,0 +1,18 @@
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+
+        return validity
